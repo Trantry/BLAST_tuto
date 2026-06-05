a4ec6a30551ac359885b94f3e6c6c1f441d98a57
Bessonnier Trystan <165653199+Trantry@users.noreply.github.com>
2026-06-05 15:51:15 +0200

    add
    This script sets up a SLURM job for running BLASTn with specified parameters, including memory, time, and output/error file paths.

---
+++ Blastn.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --array=1-6
+#SBATCH --job-name=Blastx
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --time=00-01:00:00
+#SBATCH --partition=Cascade,Genoa-premium,Emerald-premium
+#SBATCH --output=/home/tbessonn/stdout/%x_%A_%a.out
+#SBATCH --error=/home/tbessonn/stderr/%x_%A_%a.err
+
+#Set your variables her
+WORKDIR=path_to_your_workdir
+GENOME=path_to_your_genome
+
+#Make your DB (In the genome is better for blast tiny sequences)
+makeblastdb -in $GENOME -dbtype nucl
+
+mkdir -p $WORKDIR/BLAST
+cd $WORKDIR/BLAST
+
+ blastn -query /home/tbessonn/blast_white/guide_white.fasta \
+        -db $GENOME \
+        -out $WORKDIR/BLAST/Blast.outfmt6 \
+        -evalue 1e-3 \
+        -word_size 23 \
+        -perc_identity 80 \
+        -outfmt 6 \
+        -num_threads 8 \
+
+#-word_size it's the size of your sequences
+#-query it's your sequnces_file.fasta (like your geisha CRISPR)
+#-db is the thing you put in the makeblast -in THING
+
+#For searching the sequences in the genome :
+#samtools faidx $Geno Scmw4ez_472:15365203-15557205
